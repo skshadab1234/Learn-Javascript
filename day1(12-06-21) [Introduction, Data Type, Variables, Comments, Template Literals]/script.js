607837ad287430cc112f8c9f23cab009719e8b1c
skshadab1234 <ks615044@gmail.com>
2021-06-15 10:25:43 +0530

    Day 1 & Day 2 Topic Completed

---
+++ day1(12-06-21) [Introduction, Data Type, Variables, Comments, Template Literals]/script.js
@@ -0,0 +1,46 @@
+		// Variables
+			// 1. Var
+			// 2. Let
+			// 3. const
+			// let name = 'Khan';
+			// name = 'Shadab';
+			// document.write(name)
+
+		// Data Type
+			// 1. String
+				// document.write(typeof "Khan")
+
+			// 2. Number4
+				// document.write(typeof 10)
+
+			// 3. Boolean
+				// var a = true;
+				// document.write(typeof a)
+
+			// 4. Array
+				// var array = ['a','b','c','d']
+				// document.write(typeof array)
+
+			// 5. Object
+				// var object = ['a' =>'1', 'b' => 2];
+				// document.write(typeof object)
+				
+			// 6. Null
+				  // var x = null;
+				  // document.write(typeof x)	
+
+			// 7. Undefined
+					// var y;
+					// document.write(typeof y)			
+
+		// Template Literals
+			// var xyz = "Shanawaz";
+			// var x = "Shadab";
+			// var y = "Huzaif";
+
+			// document.write("This is my name: "+xyz+" and my friend name "+y+" and my lecture name "+x+"<br>")
+		
+			// document.write(`This is my name ${xyz} and my friend name ${y} and my lecture name ${x}`)
+
+
+	// document.getElementById('heading').innerHTML = ("KHAN");
