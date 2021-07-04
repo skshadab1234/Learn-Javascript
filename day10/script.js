ca17ce768905438caa0d0536a1e2ef8088e42336
skshadab1234 <59305073+skshadab1234@users.noreply.github.com>
2021-07-04 06:06:32 -0700

    Day 10 Added

---
+++ day10/script.js
@@ -0,0 +1,17 @@
+// let element = document.getElementById("header")
+
+// Get Method
+    // console.log(element.innerText)    
+    // console.log(element.innerHTML)    
+    // console.log(element.getAttribute("onclick"))
+
+    // console.log(element.getAttributeNode('onclick'))
+
+    // console.log(element.attributes[1])
+
+// Set Method
+    // console.log(element.innerText = '<H1>Home PAGE</H1>')
+    // console.log(element.innerHTML = 'Home PAGE')
+    // console.log(element.setAttribute('class','abc'))
+    // console.log(element.attributes[1].value = 'xyz')
+    // console.log(element.removeAttribute("style"))
